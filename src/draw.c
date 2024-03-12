1bb58bb95f25441efb3ef028389f3bd1fe1ac3d6
Powlar <powlarofficial@gmail.com>
2024-03-12 18:49:23 +0100

    finish norminette for 3D, parsing needed

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: nagiorgi <nagiorgi@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 17:27:16 by nagiorgi          #+#    #+#             */
-/*   Updated: 2024/03/12 18:09:01 by nagiorgi         ###   ########.fr       */
+/*   Updated: 2024/03/12 18:48:46 by nagiorgi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,48 +23,49 @@ typedef struct s_ray
 	int		wall_height;
 }	t_ray;
 
-void	draw_vertical_line(t_game *game, int x, int start_y, int end_y, unsigned int color)
+typedef struct s_coord
 {
-	if (start_y < 0)
-		start_y = 0;
+	int	x;
+	int	y;
+}	t_coord;
+
+void	draw_vertical_line(t_game *game, t_coord *start,
+	int end_y, unsigned int color)
+{
+	if (start->y < 0)
+		start->y = 0;
 	if (end_y > game->height)
 		end_y = game->height;
-	while (start_y < end_y)
+	while (start->y < end_y)
 	{
-		*(unsigned int *)(game->canvas_bytes + (x * game->canvas_bpp) + (game->canvas_line_size * start_y)) = color;
-		start_y++;
+		*(unsigned int *)(game->canvas_bytes + (start->x * game->canvas_bpp)
+				+ (game->canvas_line_size * start->y)) = color;
+		start->y++;
 	}
 }
 
-void	draw_ceiling(t_game *game, int x, int wall_height)
-{
-	draw_vertical_line(game, x, 0, game->height / 2 - wall_height, game->map.celling_color);
-}
-
-void	draw_wall(t_game *game, int x, int wall_height, double ray_x, double ray_y, t_image	*texture)
+void	draw_wall(t_game *game, int x, t_ray *ray, t_image	*texture)
 {
 	double	delta_y;
 	double	y;
 	int		max_y;
-	int		texture_y;
-	int		texture_x;
+	t_coord	tex_coord;
+	t_coord	line_start;
 
-	texture_x = (int)(texture->width * (ray_x + ray_y)) % texture->width;
-	delta_y = (double)(wall_height * 2) / (double)(texture->height);
-	y = game->height / 2 - wall_height;
-	max_y = game->height / 2 + wall_height;
-	texture_y = 0;
+	tex_coord.x = (int)(texture->width * (ray->x + ray->y)) % texture->width;
+	delta_y = (double)(ray->wall_height * 2) / (double)(texture->height);
+	y = game->height / 2 - ray->wall_height;
+	max_y = game->height / 2 + ray->wall_height;
+	tex_coord.y = 0;
 	while (y < max_y)
 	{
-		draw_vertical_line(game, x, y, y + delta_y, get_pixel(texture, texture_x, texture_y));
+		line_start.x = x;
+		line_start.y = y;
+		draw_vertical_line(game, &line_start, y + delta_y,
+			get_pixel(texture, tex_coord.x, tex_coord.y));
 		y += delta_y;
-		texture_y++;
+		tex_coord.y++;
 	}
-}
-
-void	draw_floor(t_game *game, int x, int wall_height)
-{
-	draw_vertical_line(game, x, game->height / 2 + wall_height, game->height, game->map.floor_color);
 }
 
 void	compute_ray(t_game *game, t_ray *ray)
@@ -82,14 +83,29 @@ void	compute_ray(t_game *game, t_ray *ray)
 		ray->y += ray->sin;
 		wall = map_get_at(&game->map, ray->x, ray->y);
 	}
-	ray->distance = sqrt(pow(game->player_x - ray->x, 2.0) + pow(game->player_y - ray->y, 2.0));
+	ray->distance = sqrt(pow(game->player_x - ray->x, 2.0)
+			+ pow(game->player_y - ray->y, 2.0));
 	ray->distance = ray->distance * cos(ray->angle - game->player_angle);
 	ray->wall_height = (double)(game->height) / (1.5 * ray->distance);
+}
+
+t_image	*select_texture(t_game *game, t_ray *ray)
+{
+	if (map_get_at(&game->map, ray->x, ray->y - fabs(ray->sin)) == '0')
+		return (&game->map.north);
+	else if (map_get_at(&game->map, ray->x, ray->y + fabs(ray->sin)) == '0')
+		return (&game->map.south);
+	else if (map_get_at(&game->map, ray->x + fabs(ray->cos), ray->y) == '0')
+		return (&game->map.east);
+	else if (map_get_at(&game->map, ray->x - fabs(ray->cos), ray->y) == '0')
+		return (&game->map.west);
+	return (&game->map.north);
 }
 
 void	draw_rays(t_game *game)
 {
 	t_ray	ray;
+	t_coord	coord;
 	int		x;
 	t_image	*texture;
 
@@ -98,18 +114,15 @@ void	draw_rays(t_game *game)
 	while (x < game->width)
 	{
 		compute_ray(game, &ray);
-		texture = &game->map.north;
-		if (map_get_at(&game->map, ray.x, ray.y - fabs(ray.sin)) == '0')
-			texture = &game->map.north;
-		else if (map_get_at(&game->map, ray.x, ray.y + fabs(ray.sin)) == '0')
-			texture = &game->map.south;
-		else if (map_get_at(&game->map, ray.x + fabs(ray.cos), ray.y) == '0')
-			texture = &game->map.east;
-		else if (map_get_at(&game->map, ray.x - fabs(ray.cos), ray.y) == '0')
-			texture = &game->map.west;
-		draw_ceiling(game, x, ray.wall_height);
-		draw_wall(game, x, ray.wall_height, ray.x, ray.y, texture);
-		draw_floor(game, x, ray.wall_height);
+		texture = select_texture(game, &ray);
+		coord.x = x;
+		coord.y = 0;
+		draw_vertical_line(game, &coord, game->height / 2 - ray.wall_height,
+			game->map.celling_color);
+		draw_wall(game, x, &ray, texture);
+		coord.x = x;
+		coord.y = game->height / 2 + ray.wall_height;
+		draw_vertical_line(game, &coord, game->height, game->map.floor_color);
 		ray.angle += game->angle_increment;
 		x++;
 	}
