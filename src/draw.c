515c3614cd7a752a313857d6f5d93487c0b866c9
Powlar <powlarofficial@gmail.com>
2024-03-12 18:09:59 +0100

    compute ray draw.c

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: nagiorgi <nagiorgi@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 17:27:16 by nagiorgi          #+#    #+#             */
-/*   Updated: 2024/03/12 18:02:31 by nagiorgi         ###   ########.fr       */
+/*   Updated: 2024/03/12 18:09:01 by nagiorgi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,31 +67,37 @@ void	draw_floor(t_game *game, int x, int wall_height)
 	draw_vertical_line(game, x, game->height / 2 + wall_height, game->height, game->map.floor_color);
 }
 
+void	compute_ray(t_game *game, t_ray *ray)
+{
+	char	wall;
+
+	ray->x = game->player_x;
+	ray->y = game->player_y;
+	ray->cos = cos(ray->angle) / game->precision;
+	ray->sin = sin(ray->angle) / game->precision;
+	wall = '0';
+	while (wall == '0')
+	{
+		ray->x += ray->cos;
+		ray->y += ray->sin;
+		wall = map_get_at(&game->map, ray->x, ray->y);
+	}
+	ray->distance = sqrt(pow(game->player_x - ray->x, 2.0) + pow(game->player_y - ray->y, 2.0));
+	ray->distance = ray->distance * cos(ray->angle - game->player_angle);
+	ray->wall_height = (double)(game->height) / (1.5 * ray->distance);
+}
+
 void	draw_rays(t_game *game)
 {
 	t_ray	ray;
 	int		x;
-	char	wall;
 	t_image	*texture;
 
 	ray.angle = game->player_angle - game->half_fov;
 	x = 0;
 	while (x < game->width)
 	{
-		ray.x = game->player_x;
-		ray.y = game->player_y;
-		ray.cos = cos(ray.angle) / game->precision;
-		ray.sin = sin(ray.angle) / game->precision;
-		wall = '0';
-		while (wall == '0')
-		{
-			ray.x += ray.cos;
-			ray.y += ray.sin;
-			wall = map_get_at(&game->map, ray.x, ray.y);
-		}
-		ray.distance = sqrt(pow(game->player_x - ray.x, 2.0) + pow(game->player_y - ray.y, 2.0));
-		ray.distance = ray.distance * cos(ray.angle - game->player_angle);
-		ray.wall_height = (double)(game->height) / (1.5 * ray.distance);
+		compute_ray(game, &ray);
 		texture = &game->map.north;
 		if (map_get_at(&game->map, ray.x, ray.y - fabs(ray.sin)) == '0')
 			texture = &game->map.north;
