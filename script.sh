b972149e95d1d6c6ee90d5978f0d1922d12f3367
Powlar <powlarofficial@gmail.com>
2024-04-17 15:28:01 +0200

    add script for crash test and leaks

---
+++ script.sh
@@ -0,0 +1,291 @@
+echo "./cub3D maps/bad/Bad_extension.buc"
+./cub3D maps/bad/Bad_extension.buc
+
+echo "\n./cub3D maps/bad/Bad_extensioncub"
+./cub3D maps/bad/Bad_extensioncub
+
+echo "\n./cub3D maps/bad/Double_texture_east.cub"
+./cub3D maps/bad/Double_texture_east.cub
+
+echo "\n./cub3D maps/bad/Double_texture_north.cub"
+./cub3D maps/bad/Double_texture_north.cub
+
+echo "\n./cub3D maps/bad/Double_texture_south.cub"
+./cub3D maps/bad/Double_texture_south.cub
+
+echo  "\n./cub3D maps/bad/Double_texture_west.cub"
+./cub3D maps/bad/Double_texture_west.cub
+
+echo  "\n./cub3D maps/bad/Double_texture.cub"
+./cub3D maps/bad/Double_texture.cub
+
+echo  "\n./cub3D maps/bad/Empty.cub"
+./cub3D maps/bad/Empty.cub
+
+echo  "\n./cub3D maps/bad/Hole_East_Wall.cub"
+./cub3D maps/bad/Hole_East_Wall.cub
+
+echo  "\n./cub3D maps/bad/Hole_North_Wall.cub"
+./cub3D maps/bad/Hole_North_Wall.cub
+
+echo  "\n./cub3D maps/bad/Hole_South_Wall.cub"
+./cub3D maps/bad/Hole_South_Wall.cub
+
+echo  "\n./cub3D maps/bad/Hole_West_Wall.cub"
+./cub3D maps/bad/Hole_West_Wall.cub
+
+echo  "\n./cub3D maps/bad/Incorrect_RGB_space1.cub"
+./cub3D maps/bad/Incorrect_RGB_space1.cub
+
+echo  "\n./cub3D maps/bad/Incorrect_RGB_space2.cub"
+./cub3D maps/bad/Incorrect_RGB_space2.cub
+
+echo  "\n./cub3D maps/bad/Incorrect_RGB_space3.cub"
+./cub3D maps/bad/Incorrect_RGB_space3.cub
+
+echo  "\n./cub3D maps/bad/Incorrect_RGB_tab1.cub"
+./cub3D maps/bad/Incorrect_RGB_tab1.cub
+
+echo  "\n./cub3D maps/bad/Incorrect_RGB_tab2.cub"
+./cub3D maps/bad/Incorrect_RGB_tab2.cub
+
+echo  "\n./cub3D maps/bad/Incorrect_RGB_tab3.cub"
+./cub3D maps/bad/Incorrect_RGB_tab3.cub
+
+echo  "\n./cub3D maps/bad/Incorrect_RGB.cub"
+./cub3D maps/bad/Incorrect_RGB.cub
+
+echo  "\n./cub3D maps/bad/Incorrect_RGB2.cub"
+./cub3D maps/bad/Incorrect_RGB2.cub
+
+echo  "\n./cub3D maps/bad/Isolated_char.cub"
+./cub3D maps/bad/Isolated_char.cub
+
+echo  "\n./cub3D maps/bad/Map_first.cub"
+./cub3D maps/bad/Map_first.cub
+
+echo  "\n./cub3D maps/bad/Map_middle.cub"
+./cub3D maps/bad/Map_middle.cub
+
+echo  "\n./cub3D maps/bad/Map_multiple.cub"
+./cub3D maps/bad/Map_multiple.cub
+
+echo  "\n./cub3D maps/bad/Map_space.cub"
+./cub3D maps/bad/Map_space.cub
+
+echo  "\n./cub3D maps/bad/Map_tab.cub"
+./cub3D maps/bad/Map_tab.cub
+
+echo  "\n./cub3D maps/bad/Missing_Ceiling.cub"
+./cub3D maps/bad/Missing_Ceiling.cub
+
+echo  "\n./cub3D maps/bad/Missing_Ceiling2.cub"
+./cub3D maps/bad/Missing_Ceiling2.cub
+
+echo  "\n./cub3D maps/bad/Missing_Ceiling3.cub"
+./cub3D maps/bad/Missing_Ceiling3.cub
+
+echo  "\n./cub3D maps/bad/Missing_Ceiling4.cub"
+./cub3D maps/bad/Missing_Ceiling4.cub
+
+echo  "\n./cub3D maps/bad/Missing_EA.cub"
+./cub3D maps/bad/Missing_EA.cub
+
+echo  "\n./cub3D maps/bad/Missing_FC.cub"
+./cub3D maps/bad/Missing_FC.cub
+
+echo  "\n./cub3D maps/bad/Missing_Floor.cub"
+./cub3D maps/bad/Missing_Floor.cub
+
+echo  "\n./cub3D maps/bad/Missing_Floor2.cub"
+./cub3D maps/bad/Missing_Floor2.cub
+
+echo  "\n./cub3D maps/bad/Missing_Floor3.cub"
+./cub3D maps/bad/Missing_Floor3.cub
+
+echo  "\n./cub3D maps/bad/Missing_Floor4.cub"
+./cub3D maps/bad/Missing_Floor4.cub
+
+echo  "\n./cub3D maps/bad/Missing_NO.cub"
+./cub3D maps/bad/Missing_NO.cub
+
+echo  "\n./cub3D maps/bad/Missing_RGB_C.cub"
+./cub3D maps/bad/Missing_RGB_C.cub
+
+echo  "\n./cub3D maps/bad/Missing_RGB_F.cub"
+./cub3D maps/bad/Missing_RGB_F.cub
+
+echo  "\n./cub3D maps/bad/Missing_SO.cub"
+./cub3D maps/bad/Missing_SO.cub
+
+echo  "\n./cub3D maps/bad/Missing_WE.cub"
+./cub3D maps/bad/Missing_WE.cub
+
+echo  "\n./cub3D maps/bad/Multi_player.cub"
+./cub3D maps/bad/Multi_player.cub
+
+echo  "\n./cub3D maps/bad/No_map.cub"
+./cub3D maps/bad/No_map.cub
+
+echo  "\n./cub3D maps/bad/No_player.cub"
+./cub3D maps/bad/No_player.cub
+
+echo  "\n./cub3D maps/bad/No_texture.cub"
+./cub3D maps/bad/No_texture.cub
+
+echo  "\n./cub3D maps/bad/No_Wall.cub"
+./cub3D maps/bad/No_Wall.cub
+
+echo  "\n./cub3D maps/bad/Only_map.cub"
+./cub3D maps/bad/Only_map.cub
+
+echo  "\n./cub3D maps/bad/Player_on_edge.cub"
+./cub3D maps/bad/Player_on_edge.cub
+
+echo  "\n./cub3D maps/bad/Small_map.cub"
+./cub3D maps/bad/Small_map.cub
+
+echo  "\n./cub3D maps/bad/Texture_missing.cub"
+./cub3D maps/bad/Texture_missing.cub
+
+echo  "\n./cub3D maps/bad/Texture_mixed_space_tabs.cub"
+./cub3D maps/bad/Texture_mixed_space_tabs.cub
+
+echo  "\n./cub3D maps/bad/Texture_no_xpm.cub"
+./cub3D maps/bad/Texture_no_xpm.cub
+
+echo  "\n./cub3D maps/bad/Texture_space_east.cub"
+./cub3D maps/bad/Texture_space_east.cub
+
+echo  "\n./cub3D maps/bad/Texture_space_north.cub"
+./cub3D maps/bad/Texture_space_north.cub
+
+echo  "\n./cub3D maps/bad/Texture_space_south.cub"
+./cub3D maps/bad/Texture_space_south.cub
+
+echo  "\n./cub3D maps/bad/Texture_space_west.cub"
+./cub3D maps/bad/Texture_space_west.cub
+
+echo  "\n./cub3D maps/bad/Texture_space2_east.cub"
+./cub3D maps/bad/Texture_space2_east.cub
+
+echo  "\n./cub3D maps/bad/Texture_space2_north.cub"
+./cub3D maps/bad/Texture_space2_north.cub
+
+echo  "\n./cub3D maps/bad/Texture_space2_south.cub"
+./cub3D maps/bad/Texture_space2_south.cub
+
+echo  "\n./cub3D maps/bad/Texture_space2_west.cub"
+./cub3D maps/bad/Texture_space2_west.cub
+
+echo  "\n./cub3D maps/bad/Texture_space3_east.cub"
+./cub3D maps/bad/Texture_space3_east.cub
+
+echo  "\n./cub3D maps/bad/Texture_space3_north.cub"
+./cub3D maps/bad/Texture_space3_north.cub
+
+echo  "\n./cub3D maps/bad/Texture_space3_south.cub"
+./cub3D maps/bad/Texture_space3_south.cub
+
+echo  "\n./cub3D maps/bad/Texture_space3_west.cub"
+./cub3D maps/bad/Texture_space3_west.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab_east.cub"
+./cub3D maps/bad/Texture_tab_east.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab_north.cub"
+./cub3D maps/bad/Texture_tab_north.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab_south.cub"
+./cub3D maps/bad/Texture_tab_south.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab_west.cub"
+./cub3D maps/bad/Texture_tab_west.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab2_east.cub"
+./cub3D maps/bad/Texture_tab2_east.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab2_north.cub"
+./cub3D maps/bad/Texture_tab2_north.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab2_south.cub"
+./cub3D maps/bad/Texture_tab2_south.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab2_west.cub"
+./cub3D maps/bad/Texture_tab2_west.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab3_east.cub"
+./cub3D maps/bad/Texture_tab3_east.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab3_north.cub"
+./cub3D maps/bad/Texture_tab3_north.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab3_south.cub"
+./cub3D maps/bad/Texture_tab3_south.cub
+
+echo  "\n./cub3D maps/bad/Texture_tab3_west.cub"
+./cub3D maps/bad/Texture_tab3_west.cub
+
+echo  "\n./cub3D maps/bad/Random_char_C.cub"
+./cub3D maps/bad/Random_char_C.cub
+
+echo  "\n./cub3D maps/bad/Random_char_C2.cub"
+./cub3D maps/bad/Random_char_C2.cub
+
+echo  "\n./cub3D maps/bad/Random_char_EA.cub"
+./cub3D maps/bad/Random_char_EA.cub
+
+echo  "\n./cub3D maps/bad/Random_char_EA2.cub"
+./cub3D maps/bad/Random_char_EA2.cub
+
+echo  "\n./cub3D maps/bad/Random_char_F.cub"
+./cub3D maps/bad/Random_char_F.cub
+
+echo  "\n./cub3D maps/bad/Random_char_F2.cub"
+./cub3D maps/bad/Random_char_F2.cub
+
+echo  "\n./cub3D maps/bad/Random_char_NO.cub"
+./cub3D maps/bad/Random_char_NO.cub
+
+echo  "\n./cub3D maps/bad/Random_char_NO2.cub"
+./cub3D maps/bad/Random_char_NO2.cub
+
+echo  "\n./cub3D maps/bad/Random_char_SO.cub"
+./cub3D maps/bad/Random_char_SO.cub
+
+echo  "\n./cub3D maps/bad/Random_char_SO2.cub"
+./cub3D maps/bad/Random_char_SO2.cub
+
+echo  "\n./cub3D maps/bad/Random_char_WE.cub"
+./cub3D maps/bad/Random_char_WE.cub
+
+echo  "\n./cub3D maps/bad/Random_char_WE2.cub"
+./cub3D maps/bad/Random_char_WE2.cub
+
+echo  "\n./cub3D maps/bad/Wrong_RGB_1.cub"
+./cub3D maps/bad/Wrong_RGB_1.cub
+
+echo  "\n./cub3D maps/bad/Wrong_RGB_2.cub"
+./cub3D maps/bad/Wrong_RGB_2.cub
+
+echo  "\n./cub3D maps/bad/Wrong_RGB_3.cub"
+./cub3D maps/bad/Wrong_RGB_3.cub
+
+echo  "\n./cub3D maps/bad/Wrong_xpm_EA.cub"
+./cub3D maps/bad/Wrong_xpm_EA.cub
+
+echo  "\n./cub3D maps/bad/Wrong_xpm_NO.cub"
+./cub3D maps/bad/Wrong_xpm_NO.cub
+
+echo  "\n./cub3D maps/bad/Wrong_xpm_SO.cub"
+./cub3D maps/bad/Wrong_xpm_SO.cub
+
+echo  "\n./cub3D maps/bad/Wrong_xpm_WE.cub"
+./cub3D maps/bad/Wrong_xpm_WE.cub
+
+echo  "\n./cub3D maps/bad/Map_char_after_wall.cub"
+./cub3D maps/bad/Map_char_after_wall.cub
+
+echo  "\n./cub3D maps/bad/Map_tab_after_wall.cub"
+./cub3D maps/bad/Map_tab_after_wall.cub
+
---
+++ script_leaks.sh
@@ -0,0 +1,288 @@
+echo "\nleaks --atExit -- ./cub3D /maps/bad/Bad_extension.buc"
+leaks --atExit -- ./cub3D /maps/bad/Bad_extension.buc
+
+echo "\nleaks --atExit -- ./cub3D /maps/bad/Bad_extensioncub"
+leaks --atExit -- ./cub3D /maps/bad/Bad_extensioncub
+
+echo "\nleaks --atExit -- ./cub3D /maps/bad/Double_texture_east.cub"
+leaks --atExit -- ./cub3D /maps/bad/Double_texture_east.cub
+
+echo "\nleaks --atExit -- ./cub3D /maps/bad/Double_texture_north.cub"
+leaks --atExit -- ./cub3D /maps/bad/Double_texture_north.cub
+
+echo "\nleaks --atExit -- ./cub3D /maps/bad/Double_texture_south.cub"
+leaks --atExit -- ./cub3D /maps/bad/Double_texture_south.cub
+
+echo  "\nleaks --atExit --  maps/bad/Double_texture_west.cub"
+leaks --atExit -- ./cub3D /maps/bad/Double_texture_west.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Double_texture.cub"
+leaks --atExit -- ./cub3D /maps/bad/Double_texture.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Empty.cub"
+leaks --atExit -- ./cub3D /maps/bad/Empty.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Hole_East_Wall.cub"
+leaks --atExit -- ./cub3D /maps/bad/Hole_East_Wall.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Hole_North_Wall.cub"
+leaks --atExit -- ./cub3D /maps/bad/Hole_North_Wall.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Hole_South_Wall.cub"
+leaks --atExit -- ./cub3D /maps/bad/Hole_South_Wall.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Hole_West_Wall.cub"
+leaks --atExit -- ./cub3D /maps/bad/Hole_West_Wall.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_space1.cub"
+leaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_space1.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_space2.cub"
+leaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_space2.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_space3.cub"
+leaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_space3.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_tab1.cub"
+leaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_tab1.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_tab2.cub"
+leaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_tab2.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_tab3.cub"
+leaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB_tab3.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB.cub"
+leaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB2.cub"
+leaks --atExit -- ./cub3D /maps/bad/Incorrect_RGB2.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Isolated_char.cub"
+leaks --atExit -- ./cub3D /maps/bad/Isolated_char.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Map_first.cub"
+leaks --atExit -- ./cub3D /maps/bad/Map_first.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Map_middle.cub"
+leaks --atExit -- ./cub3D /maps/bad/Map_middle.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Map_multiple.cub"
+leaks --atExit -- ./cub3D /maps/bad/Map_multiple.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Map_space.cub"
+leaks --atExit -- ./cub3D /maps/bad/Map_space.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Map_tab.cub"
+leaks --atExit -- ./cub3D /maps/bad/Map_tab.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_Ceiling.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_Ceiling.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_Ceiling2.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_Ceiling2.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_Ceiling3.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_Ceiling3.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_Ceiling4.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_Ceiling4.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_EA.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_EA.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_FC.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_FC.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_Floor.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_Floor.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_Floor2.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_Floor2.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_Floor3.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_Floor3.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_Floor4.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_Floor4.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_NO.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_NO.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_RGB_C.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_RGB_C.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_RGB_F.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_RGB_F.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_SO.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_SO.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Missing_WE.cub"
+leaks --atExit -- ./cub3D /maps/bad/Missing_WE.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Multi_player.cub"
+leaks --atExit -- ./cub3D /maps/bad/Multi_player.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/No_map.cub"
+leaks --atExit -- ./cub3D /maps/bad/No_map.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/No_player.cub"
+leaks --atExit -- ./cub3D /maps/bad/No_player.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/No_texture.cub"
+leaks --atExit -- ./cub3D /maps/bad/No_texture.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/No_Wall.cub"
+leaks --atExit -- ./cub3D /maps/bad/No_Wall.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Only_map.cub"
+leaks --atExit -- ./cub3D /maps/bad/Only_map.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Player_on_edge.cub"
+leaks --atExit -- ./cub3D /maps/bad/Player_on_edge.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Small_map.cub"
+leaks --atExit -- ./cub3D /maps/bad/Small_map.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_missing.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_missing.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_mixed_space_tabs.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_mixed_space_tabs.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_no_xpm.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_no_xpm.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space_east.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space_east.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space_north.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space_north.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space_south.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space_south.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space_west.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space_west.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space2_east.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space2_east.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space2_north.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space2_north.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space2_south.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space2_south.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space2_west.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space2_west.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space3_east.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space3_east.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space3_north.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space3_north.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space3_south.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space3_south.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_space3_west.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_space3_west.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab_east.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab_east.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab_north.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab_north.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab_south.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab_south.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab_west.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab_west.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab2_east.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab2_east.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab2_north.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab2_north.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab2_south.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab2_south.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab2_west.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab2_west.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab3_east.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab3_east.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab3_north.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab3_north.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab3_south.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab3_south.cub
+
+echo  "\nleaks --atExit -- ./cub3D /maps/bad/Texture_tab3_west.cub"
+leaks --atExit -- ./cub3D /maps/bad/Texture_tab3_west.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_C.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_C.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_C2.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_C2.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_EA.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_EA.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_EA2.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_EA2.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_F.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_F.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_F2.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_F2.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_NO.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_NO.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_NO2.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_NO2.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_SO.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_SO.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_SO2.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_SO2.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_WE.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_WE.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Random_char_WE2.cub"
+leaks --atExit -- ./cub3D maps/bad/Random_char_WE2.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Wrong_RGB_1.cub"
+leaks --atExit -- ./cub3D maps/bad/Wrong_RGB_1.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Wrong_RGB_2.cub"
+leaks --atExit -- ./cub3D maps/bad/Wrong_RGB_2.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Wrong_RGB_3.cub"
+leaks --atExit -- ./cub3D maps/bad/Wrong_RGB_3.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Wrong_xpm_EA.cub"
+leaks --atExit -- ./cub3D maps/bad/Wrong_xpm_EA.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Wrong_xpm_NO.cub"
+leaks --atExit -- ./cub3D maps/bad/Wrong_xpm_NO.cub
+
+echo  "\nleaks --atExit -- ./cub3D maps/bad/Wrong_xpm_SO.cub"
+leaks --atExit -- ./cub3D maps/bad/Wrong_xpm_SO.cub
+
+echo  "\n./cub3D maps/bad/Map_char_after_wall.cub"
+leaks --atExit -- ./cub3D maps/bad/Map_char_after_wall.cub
+
+echo  "\n./cub3D maps/bad/Map_tab_after_wall.cub"
+leaks --atExit -- ./cub3D maps/bad/Map_tab_after_wall.cub
+
